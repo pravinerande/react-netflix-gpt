5de5a31c44035f4a0e6a445ae8a82db250c8e330
Pravin <pravin@securly.com>
2026-01-02 13:15:12 +0530

    Local files commit scaffolding

---
+++ README.md
@@ -0,0 +1,15 @@
+# Netflix GPT
+- create create app
+- tailwind css integration
+
+
+# Features
+ - login
+   - singup /singin
+ - Browse (after authentication)
+    - Header
+        - Main movie
+        - Movies Suggestions
+ - Netflix GPT
+  - search bar
+  - suggestions
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="font-bold italic text-3xl text-green-500">Netflix GPT</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
